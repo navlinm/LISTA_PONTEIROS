29a17aa171925ad699ed36fdff1850bee2dd2446
navlinm <94932697+navlinm@users.noreply.github.com>
2021-11-23 21:48:44 -0300

    Create Q03.c

---
+++ Q03.c
@@ -0,0 +1,10 @@
+3.	Mostre o que será impresso por programa supondo que i ocupa o endereço 4094 na memória.
+  int i=5, *p;
+  p = &i;
+  printf("%x %d %d %d %d", p, *p+2, **&p, 3**p, *&p+4);
+
+p=4094
+*p+2=5+2=7
+**&p=5
+3**p=3*5=15
+**&p+4=5+4=9
