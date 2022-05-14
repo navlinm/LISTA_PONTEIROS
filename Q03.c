9239608c3dd4b5179c6412e4016efc6f1b948c37
navlinm <94932697+navlinm@users.noreply.github.com>
2022-05-14 18:08:57 -0300

    Create Q03.c

---
+++ Q03.c
@@ -0,0 +1,11 @@
+3.
+Se i e j são variáveis inteiras e p e q ponteiros para int, quais das seguintes expressões de atribuição são ilegais? 
+
+ p = &i; 
+ q = &j; 
+ p = &*&i;
+ i = (*&)j; ilegal
+ i = *&j; 
+ i = *&*&j; ilegal 
+ q = *p; ilegal 
+ i = (*p)++ + *q; ilegal
