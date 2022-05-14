872b59cebdf9b9343a8d02c5723ac0a34e73b967
navlinm <94932697+navlinm@users.noreply.github.com>
2022-05-14 18:14:42 -0300

    Create Q11.c

---
+++ Q11.c
@@ -0,0 +1,14 @@
+11.
+  Suponha que as seguintes declarações tenham sido realizadas:
+  float aloha[10], coisas[10][5], *pf, value=2.2;
+  int i=3;
+Identifique quais dos seguintes comandos é válido ou inválido: 
+ 
+a. aloha[2] = value; valido
+b. scanf("%f", &aloha); valido
+c. aloha = value; invalido
+d. printf("%f", aloha); valido
+e. coisas[4][4] = aloha[3]; valido
+f. coisas[5] = aloha; invalido
+g. pf = value; invalido
+h. pf = aloha; valido
