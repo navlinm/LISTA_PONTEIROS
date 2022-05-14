93760c910f73357ba824c6bda2d658d3d01aa199
navlinm <94932697+navlinm@users.noreply.github.com>
2022-05-14 18:07:09 -0300

    Create Q01.c

---
+++ Q01.c
@@ -0,0 +1,12 @@
+1.
+Seja o seguinte trecho de programa:
+  int i=3, j=5;
+  int *p, *q;
+  p = &i;
+  q = &j
+Determine o valor das seguintes expressões:
+
+◦ p == &i; TRUE 
+◦ *p - *q; =-2
+◦ **&p; =3
+◦ 3 - *p/(*q) + 7; =10, pois a divisão vai ser igual a 0, as variáveis são expressas em int.
