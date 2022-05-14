208cacbe8227ddc6b26490e6410b5e8bbcd8232d
navlinm <94932697+navlinm@users.noreply.github.com>
2022-05-14 18:11:00 -0300

    Create Q06.c

---
+++ Q06.c
@@ -0,0 +1,7 @@
+6.
+Assumindo que pulo[] é um vetor do tipo int, quais das seguintes expressões referenciam o valor do terceiro elemento do vetor? 
+
+◦ *(pulo + 2); este 
+◦ *(pulo + 4);
+◦ pulo + 4; 
+◦ pulo + 2; 
