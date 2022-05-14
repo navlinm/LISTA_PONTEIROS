7549ca9fac177c2de6124c6468566bacaad1d568
navlinm <94932697+navlinm@users.noreply.github.com>
2022-05-14 18:12:36 -0300

    Create Q07.c

---
+++ Q07.c
@@ -0,0 +1,9 @@
+7.
+Considerando a declaração
+  int mat[4], *p, x;
+quais das seguintes expressões são válidas? Justifique. 
+
+◦ p = mat + 1; valida, pois mat[x]+1 = mat[x+1], consequentemente é valido
+◦ p = mat++;  valida, pois ela atribui o valor de mat à p
+◦ p =++mat; invalida, pois os "++" esta antes do mat
+◦ x = (*mat);` valida, pois o conteúdo armazenado em mat vai para x e a aspa está após o fim da linha
